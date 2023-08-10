42496ebed77e2acdf92fcc248a767fac277a4822
Виктор <vitya1313vitya@gmail.com>
2023-08-10 20:53:54 +0300

    Add has_permission method to custom permission.

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -6,8 +6,17 @@ class IsCreatorOrReadOnly(permissions.BasePermission):
     Проверяет является ли отправитель запроса автором определенного поста.
     """
 
+    def has_permission(self, request, view):
+        """Фунцкия для проверки запроса от различных пользователей."""
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
-        """Фунцкия для проверки пользователя"""
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        """Фунцкия для проверки пользователя."""
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
